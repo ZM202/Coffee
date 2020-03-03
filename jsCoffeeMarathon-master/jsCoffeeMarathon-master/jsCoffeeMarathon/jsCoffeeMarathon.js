a7ff74ab3012a65b4ceea08ee777ad421fa21030
ZM <48497724+ZM202@users.noreply.github.com>
2020-03-03 09:02:11 -0500

    Add files via upload

---
+++ jsCoffeeMarathon-master/jsCoffeeMarathon-master/jsCoffeeMarathon/jsCoffeeMarathon.js
@@ -0,0 +1,37 @@
+console.log("Coffee Please!");
+let coffeeType="Medium Roast";
+console.log(coffeeType);
+
+var latte=2;
+var mocha=3;
+console.log(latte+mocha);
+
+console.log(latte>mocha);
+console.log(latte<mocha);
+
+let sugarfree;
+alert(sugarfree);
+window.alert(sugarfree);
+
+let customer1 = "Kamala Khan", drinkChoice = "Chai", Quantity = 4, foodAllergy ="False";
+console.log(`${customer1} ${drinkChoice} ${Quantity} ${foodAllergy}`);
+
+const chai=2.50
+console.log(chai*quantity);
+
+let chai=2;
+var chai=2;
+console.log(chai);
+
+
+let total="your final order:"+ drinkChoice+ " " + Quantity + " " + $ + " " + Quantity*Chai;
+
+alert ("your final order: " + total);
+
+let total="your final order:"+ drinkChoice+ " " + "for a total of $" + Quantity + " " + $ + " " + Quantity*Chai;
+
+alert ("your final order: " + total);
+
+console.log ()
+
+
